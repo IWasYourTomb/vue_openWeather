4f0e17b3239beb00f8b90b662a2c2ca12ce4efb2
IWasYourTomb <wowzik1233@gmail.com>
2022-06-14 16:29:15 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
         </div>
         <div class="weather-box">
           <div class="temp"> {{ Math.round(weather.main.temp) }}°C</div>
-          <div class="weather"> {{ weather.weather[0].main }} °C</div>
+          <div class="weather"> {{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
